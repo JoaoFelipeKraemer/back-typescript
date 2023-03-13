92e98d0f04eaff7370d103c9fb33c371dbda97f8
João Felipe Kraemer Silveira <jo_oaoo@hotmail.com>
2023-03-13 00:59:09 -0300

    dia 1 exercicios feitos

--- my-ts-scripts/length.ts
+++ my-ts-scripts/length.ts
@@ -1,6 +1,7 @@
 // ./length.ts
+// lembrete: mudar o array units e a base para o cálculo em cada arquivo!
 
-
+import readline from 'readline-sync'; // importamos o pacote readline-sync
 const units = ['km', 'hm', 'dam', 'm', 'dm', 'cm', 'mm'];
 
 function convert(value: number, fromUnit: string, toUnit: string): number {
@@ -11,15 +12,36 @@ function convert(value: number, fromUnit: string, toUnit: string): number {
   return value * Math.pow(10, exponent);
 }
 
+function exec() {
+  // pegamos o valor a ser convertido digitado pela pessoa usuária
+  const value = readline.questionFloat('Digite o valor a ser convertido: \n');
+
+  // pedimos que a pessoa usuária escolha a unidade base
+  const fromUnitChoiceIndex = readline.keyInSelect(units, 'Escolha um número para a unidade base:');
+
+  // pedimos que a pessoa usuária escolha a unidade para conversão
+  const toUnitChoiceIndex = readline.keyInSelect(units, 'Escolha um número para a conversão:');
+
+  const toUnitChoice = units[toUnitChoiceIndex];
+  const fromUnitChoice = units[fromUnitChoiceIndex];
+
+  // Se o usuário escolher a opção 0 (cancelar), uma mensagem é impressa no terminal e usamos o return para encerrar a execução
+  if (!fromUnitChoice || !toUnitChoice) {
+    console.log(`Função cancelada`);
+    return 0;
+  }
+  
+  const result = convert(value, fromUnitChoice, toUnitChoice);
+
+  // montamos a mensagem de saída
+  const message = `${value}${fromUnitChoice} é igual a ${result}${toUnitChoice}`;
+
+  // printamos a mensagem de saída no terminal
+  console.log(message);
+}
+
+exec();
 // ./length.ts
 
-const unitsmass = ['kg', 'hg', 'dag', 'g', 'dg', 'cg', 'mg'];
 
-function convertMass(value: number, fromUnit: string, toUnit: string): number {
-  const fromIndex = unitsmass.indexOf(fromUnit); // pegamos o index da unidade base no array
-  const toIndex = unitsmass.indexOf(toUnit); // pegamos o index da unidade para a conversão
-  const exponent = toIndex - fromIndex; // calculamos o expoente a partir da diferença dos index
-
-  return value * Math.pow(10, exponent);
-}
 
